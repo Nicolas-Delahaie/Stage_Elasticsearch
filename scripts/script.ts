c16d62bc67f5ee7764a29c3ab9dd046e81afad1d
Matis Chabanat <m.chabanat@solidpepper.com>
2024-03-11 17:05:03 +0100

    uniformisation du nom des propriétés + separation des langues + séparation de l'analyseur pour le stockage et pour les embeddings + generation des strings réduits pour l'embeddings

--- scripts/script.ts
+++ scripts/script.ts
@@ -2,10 +2,9 @@ import fs from "fs";
 import { Client } from "@elastic/elasticsearch";
 
 type T_sku = {
-  id: string;
-  name: string;
-  description: string;
-  weight: number;
+  skuGuid: string;
+  skuName: string;
+  skuDescription: string;
 };
 
 const RESULTS_SIZE = 100;
@@ -34,9 +33,8 @@ class Elasticsearch extends Client {
   public async Initialisation() {
     try {
       await this.deleteIndex();
-      // await this.createAnalyzer();
       await this.createIndex();
-      await this.indexDocuments();
+      await this.indexDocuments(5);
       console.log("Initialisation reussie !");
     } catch (error) {
       console.error(error);
@@ -58,53 +56,58 @@ class Elasticsearch extends Client {
       }
     }
   }
-  private async createAnalyzer() {
-    const res = await this.indices.create({
-      index: this.INDEX_NAME,
-      settings: {
-        analysis: {
-          char_filter: {
-            cleaner: {
-              type: "pattern_replace",
-              pattern:
-                "(\\* Offre de bienvenue 5% de réduction sur votre 1ère commande avec le code: PROMO5)|\
-                  (- Offre -5%)|\
-                  (A partir [0-9]+ € D'ACHAT = [0-9]+% DE REMISE-code promo OFFRE[0-9]+)|\
-                  (Offre de Bienvenue : 5% avec le code promo : PROMO5)|\
-                  (DERNIÈRE DEMARQUE -10% SUPPLEMENTAIRES.*AVEC LE CODE PROMO : 10)",
-              replacement: "",
-            },
-          },
-          analyzer: {
-            french_analyzer: {
-              type: "custom",
-              tokenizer: "standard",
-              filter: ["lowercase", "asciifolding"],
-              char_filter: ["html_strip", "cleaner"],
-            },
-          },
-        },
-      },
-    });
-  }
   private async createIndex() {
     const res = await this.indices.create({
       index: this.INDEX_NAME,
       body: {
         mappings: {
           properties: {
-            id: { type: "text" },
-            name: { type: "text", analyzer: "french" },
-            description: {
-              type: "text",
-              analyzer: "french",
+            skuGuid: { type: "text" },
+            skuName: {
+              properties: {
+                fr: { type: "text", analyzer: "french" },
+                en: { type: "text", analyzer: "english" },
+                es: { type: "text", analyzer: "spanish" },
+                ge: { type: "text", analyzer: "german" },
+              },
+            },
+            skuDescription: {
+              properties: {
+                fr: { type: "text", analyzer: "french" },
+                en: { type: "text", analyzer: "english" },
+                es: { type: "text", analyzer: "spanish" },
+                ge: { type: "text", analyzer: "german" },
+              },
             },
             embedding: {
               type: "dense_vector",
               dims: 2,
               index: true,
             },
-            weight: { type: "float" },
+          },
+        },
+        settings: {
+          analysis: {
+            analyzer: {
+              embedding_analyzer: {
+                type: "custom",
+                tokenizer: "standard",
+                filter: [
+                  "french_stop_words",
+                  "asciifolding",
+                  "remove_duplicates",
+                  "lowercase",
+                  "elision",
+                ],
+              },
+            },
+            filter: {
+              french_stop_words: {
+                type: "stop",
+                ignore_case: true,
+                stopwords: ["_french_"],
+              },
+            },
           },
         },
       },
@@ -114,25 +117,39 @@ class Elasticsearch extends Client {
       throw new Error("Création index ratée");
     }
   }
+  private cleaner(text: string) {
+    const regexPromos =
+      /(\* Offre de bienvenue 5% de réduction sur votre 1ère commande avec le code: PROMO5)|(- Offre -5%)|(A partir [0-9]+ € D'ACHAT = [0-9]+% DE REMISE-code promo OFFRE[0-9]+)|(Offre de Bienvenue : 5% avec le code promo : PROMO5)|(DERNIÈRE DEMARQUE -10% SUPPLEMENTAIRES.*AVEC LE CODE PROMO : 10)/;
+    const regexDimensions = /[0-9]+x[0-9]+( cm)?/;
+    const regexBalises = /<[^>]*>/g;
+    const regexNombres = /[0-9]+ ?(€|°|%)?/g;
+    text = text.replace(regexPromos, "");
+    text = text.replace(regexDimensions, "");
+    text = text.replace(regexBalises, " ");
+    text = text.replace(regexNombres, "");
+    return text;
+  }
   private async indexDocuments(docsNumber?: number | undefined) {
     const skusFile = fs.readFileSync("exemple_donnees.json", "utf8");
     const skusBrut = JSON.parse(skusFile).skus as any[];
-    const skus = skusBrut.map((sku) => {
-      const formatedSku: T_sku = {
-        id: sku.skuGuid,
-        name: sku.skuName.fr ?? "" /**@todo Langue à gérer */,
-        description: sku.skuDescription.fr ?? "" /**@todo Langue à gérer */,
-        weight: sku.skuWeight,
-      };
-      return formatedSku;
-    });
+    const skus = skusBrut.map((sku) => ({
+      skuGuid: sku.skuGuid,
+      skuDescription: {
+        ...sku.skuDescription,
+        fr: this.cleaner(sku.skuDescription.fr ?? ""),
+      },
+      skuName: {
+        ...sku.skuName,
+        fr: this.cleaner(sku.skuName.fr ?? ""),
+      },
+    }));
 
     if (docsNumber) {
       skus.splice(docsNumber);
     }
 
     let operations: any[] = [];
-    skus.forEach((sku) => {
+    skus.forEach((sku: T_sku) => {
       operations.push({ create: { _index: this.INDEX_NAME } });
       operations.push(sku);
     });
@@ -142,6 +159,7 @@ class Elasticsearch extends Client {
       /**@todo intégrer un analyseur */
       operations: operations,
     });
+    console.log(res);
 
     if (res.errors) {
       throw new Error(
@@ -208,12 +226,43 @@ const etk = new Elasticsearch();
   // );
 
   // console.log(
-  //   await etk.indices.analyze({
-  //     // index: etk.INDEX_NAME,
-  //     body: {
-  //       text: '<p class="p1"><span style="font-size: 11.8181819915771px;"><br></span></p><p class="p1"><span style="font-size: 11.8181819915771px;">Le lit junior Leander a été primé à de nombreuses reprises pour son design astucieux et fonctionnel.<br>Tout en rondeur, le designer Stig Leander Nielsen utilise la technique scandinave du bois cintré pour créer une forme de mobilier "organique" qui se veut chaleureuse et rassurante.</span><br></p><p class="p1">Le matelas inclus, est en mousse haute résilience, coutil en laine thermo ouatinée traité écotex. La couverture du matelas a suivi un processus hypo-allergénique, elle est lavable à 60°C.&nbsp;</p><p class="p1"><span style="font-size: 11.8181819915771px;">Il accueille votre enfant&nbsp;</span><span style="font-size: 11.8181819915771px;">jusqu\'à l\'âge de 8 ans</span><span style="font-size: 11.8181819915771px;">.</span><br></p><p class="p1"><span style="font-size: 11.8181819915771px;">La barrière de sécurité qui s\'adapte parfaitement au lit junior (70x150 cm) est vendue séparément rubrique: Accessoires lit enfant.</span></p><p class="p1"><br><br></p>',
-  //       analyzer: "french_analyzer",
-  //     },
-  //   })
+  //   (
+  //     await etk.indices.analyze({
+  //       index: etk.INDEX_NAME,
+  //       body: {
+  //         text: [
+  //           "Joli voile  % coton aufinitions soignées. Il s'adapte à tous les berceaux et lits bébé Troll Nursery.Lavable en machine à °.Se fixe à la flèche de lit présentée dans la même rubrique.",
+  //           "Essentiel pour préserver son matelas, l'alèse spécialement adaptée au matelas berceau TROLL.Coloris écru. % coton waterproof, intérieur  % polyester, bordures avec élastique  Lavable en machine à",
+  //         ],
+  //         analyzer: "embedding_analyzer",
+  //       },
+  //     })
+  //   ).tokens?.map((token) => token.token)
   // );
+
+  async function embeddingAnalyzer(texts: string[]) {
+    const tokensPromises = texts.map(async (text) => {
+      const res = await etk.indices.analyze({
+        index: etk.INDEX_NAME,
+        body: {
+          text,
+          analyzer: "embedding_analyzer",
+        },
+      });
+      if (!res.tokens) {
+        throw new Error("Analyseur d'embedding a écouché");
+      }
+      const tokens = res.tokens.map((token) => token.token);
+      const tokensJoined = tokens.join(" ");
+      return tokensJoined;
+    });
+    return await Promise.all(tokensPromises);
+  }
+
+  const texts = [
+    "Essentiel pour préserver son matelas, l'alèse spécialement adaptée au matelas berceau TROLL. Coloris écru.  coton waterproof, intérieur  polyester, bordures avec élastique   Lavable en machine à   ",
+    "Flèche de lit Universelle Troll Nursery. Elle s'adapte à tous les berceaux et lits bébé Troll Nursery (à l'exception des berceaux textile).  Réglable en hauteur. Tube en acier laqué blanc avec pinces crocodile.  A associer au voile universel Troll (même rubrique)",
+  ];
+
+  console.log(await embeddingAnalyzer(texts));
 })();
