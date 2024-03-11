9a023a0a20df0c001ba6ee7301fcd9a77dbd2fae
Matis Chabanat <m.chabanat@solidpepper.com>
2024-03-11 12:15:16 +0100

    Refactor code avec des throw error

--- scripts/script.ts
+++ scripts/script.ts
@@ -1,6 +1,5 @@
 import fs from "fs";
 import { Client } from "@elastic/elasticsearch";
-import { MappingTypeMapping } from "@elastic/elasticsearch/lib/api/types";
 
 type T_sku = {
   id: string;
@@ -9,14 +8,13 @@ type T_sku = {
   weight: number;
 };
 
-const RESULTS_SIZE = 5;
+const RESULTS_SIZE = 100;
 
-class Elasticsearch {
-  private client: Client;
-  private readonly INDEX_NAME: string = "skus";
+class Elasticsearch extends Client {
+  public readonly INDEX_NAME: string = "skus";
 
   constructor() {
-    this.client = new Client({
+    super({
       node: "https://localhost:9200", // Elasticsearch endpoint
       auth: {
         // apiKey: {
@@ -33,39 +31,64 @@ class Elasticsearch {
     });
   }
 
-  public async Initialisation(): Promise<boolean> {
-    const initOk =
-      (await this.deleteIndex()) &&
-      (await this.createIndex()) &&
-      (await this.indexDocuments(5));
-
-    console.log(
-      initOk ? "Initialisation reussie !" : "Initialisation ratée..."
-    );
-
-    return initOk;
+  public async Initialisation() {
+    try {
+      await this.deleteIndex();
+      // await this.createAnalyzer();
+      await this.createIndex();
+      await this.indexDocuments();
+      console.log("Initialisation reussie !");
+    } catch (error) {
+      console.error(error);
+    }
   }
-  private async deleteIndex(): Promise<boolean> {
-    const existeDeja = await this.client.indices.exists({
+  private async deleteIndex() {
+    const existeDeja = await this.indices.exists({
       index: this.INDEX_NAME,
     });
 
     if (existeDeja) {
-      const res = await this.client.indices.delete({
+      const res = await this.indices.delete({
         index: this.INDEX_NAME,
         ignore_unavailable: true,
       });
 
       if (!res.acknowledged) {
-        console.log(res);
+        throw new Error("Suppression ancien index raté");
       }
-      return res.acknowledged;
-    } else {
-      return true;
     }
   }
-  private async createIndex(): Promise<boolean> {
-    const res = await this.client.indices.create({
+  private async createAnalyzer() {
+    const res = await this.indices.create({
+      index: this.INDEX_NAME,
+      settings: {
+        analysis: {
+          char_filter: {
+            cleaner: {
+              type: "pattern_replace",
+              pattern:
+                "(\\* Offre de bienvenue 5% de réduction sur votre 1ère commande avec le code: PROMO5)|\
+                  (- Offre -5%)|\
+                  (A partir [0-9]+ € D'ACHAT = [0-9]+% DE REMISE-code promo OFFRE[0-9]+)|\
+                  (Offre de Bienvenue : 5% avec le code promo : PROMO5)|\
+                  (DERNIÈRE DEMARQUE -10% SUPPLEMENTAIRES.*AVEC LE CODE PROMO : 10)",
+              replacement: "",
+            },
+          },
+          analyzer: {
+            french_analyzer: {
+              type: "custom",
+              tokenizer: "standard",
+              filter: ["lowercase", "asciifolding"],
+              char_filter: ["html_strip", "cleaner"],
+            },
+          },
+        },
+      },
+    });
+  }
+  private async createIndex() {
+    const res = await this.indices.create({
       index: this.INDEX_NAME,
       body: {
         mappings: {
@@ -88,13 +111,10 @@ class Elasticsearch {
     });
 
     if (!res.acknowledged) {
-      console.log(res);
+      throw new Error("Création index ratée");
     }
-    return res.acknowledged;
   }
-  private async indexDocuments(
-    docsNumber: number | undefined
-  ): Promise<boolean> {
+  private async indexDocuments(docsNumber?: number | undefined) {
     const skusFile = fs.readFileSync("exemple_donnees.json", "utf8");
     const skusBrut = JSON.parse(skusFile).skus as any[];
     const skus = skusBrut.map((sku) => {
@@ -117,21 +137,26 @@ class Elasticsearch {
       operations.push(sku);
     });
 
-    const res = await this.client.bulk({
+    const res = await this.bulk({
       refresh: true,
       /**@todo intégrer un analyseur */
       operations: operations,
     });
 
     if (res.errors) {
-      console.log(res);
+      throw new Error(
+        "Indexation ratée : " +
+          res.took +
+          " documents indexés sur " +
+          skus.length
+      );
     }
-    return !res.errors;
   }
+  private async generateEmbeddings() {}
 
   // TESTING
   public async testSearch() {
-    return await this.client.search({
+    return await this.search({
       index: this.INDEX_NAME,
       query: {
         bool: {
@@ -162,13 +187,33 @@ class Elasticsearch {
 }
 
 const etk = new Elasticsearch();
-etk.Initialisation();
 (async () => {
+  await etk.Initialisation();
+
   /* Getters */
   // console.log((await client.indices.get({ index: INDEX_NAME }))[INDEX_NAME]);
   // console.log((await client.indices.getMapping()).skus);
   // console.log(
-  //   (await client.search({ index: INDEX_NAME, size: 10000 })).hits.hits
+  //   (await etk.indices.getSettings({ index: "_all" }))?.skus?.settings?.index
   // );
   // console.log((await searchSkus()).hits.hits);
+  // console.log(
+  //   (
+  //     await etk.termvectors({
+  //       index: etk.INDEX_NAME,
+  //       id: "UarZLI4Bq2pXlB88xHEN",
+  //       fields: ["description", "name"],
+  //     })
+  //   ).term_vectors?.description?.terms
+  // );
+
+  // console.log(
+  //   await etk.indices.analyze({
+  //     // index: etk.INDEX_NAME,
+  //     body: {
+  //       text: '<p class="p1"><span style="font-size: 11.8181819915771px;"><br></span></p><p class="p1"><span style="font-size: 11.8181819915771px;">Le lit junior Leander a été primé à de nombreuses reprises pour son design astucieux et fonctionnel.<br>Tout en rondeur, le designer Stig Leander Nielsen utilise la technique scandinave du bois cintré pour créer une forme de mobilier "organique" qui se veut chaleureuse et rassurante.</span><br></p><p class="p1">Le matelas inclus, est en mousse haute résilience, coutil en laine thermo ouatinée traité écotex. La couverture du matelas a suivi un processus hypo-allergénique, elle est lavable à 60°C.&nbsp;</p><p class="p1"><span style="font-size: 11.8181819915771px;">Il accueille votre enfant&nbsp;</span><span style="font-size: 11.8181819915771px;">jusqu\'à l\'âge de 8 ans</span><span style="font-size: 11.8181819915771px;">.</span><br></p><p class="p1"><span style="font-size: 11.8181819915771px;">La barrière de sécurité qui s\'adapte parfaitement au lit junior (70x150 cm) est vendue séparément rubrique: Accessoires lit enfant.</span></p><p class="p1"><br><br></p>',
+  //       analyzer: "french_analyzer",
+  //     },
+  //   })
+  // );
 })();
