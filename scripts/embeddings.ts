ca425f4b10791f98e6dc2609717ac45d54ada54e
Nicolas delahaie <n.delahaie@solidpepper.com>
2024-03-13 15:33:05 +0100

    Suppression fichier d'embedding

--- scripts/embeddings.ts
+++ scripts/embeddings.ts
@@ -4,11 +4,7 @@ const FILE_NAME = "generated_embeddings.json";
 const MODEL_NAME = "text-embedding-3-small";
 
 // ----------------- FONCTIONS ----------------- //
-export async function embeddingApi(
-  texts: string[],
-  dimensions: number,
-  save = false
-) {
+export async function embeddingApi(texts: string[], dimensions: number) {
   const response = await fetch("https://api.openai.com/v1/embeddings", {
     headers: {
       Authorization:
@@ -38,12 +34,6 @@ export async function embeddingApi(
     // Sauvegarde des resultats
     const results = brutResults.data.map((data) => data.embedding);
 
-    if (save === true) {
-      const currentEmbeddings = JSON.parse(fs.readFileSync(FILE_NAME, "utf8"));
-      const newEmbeddings = JSON.stringify([...currentEmbeddings, ...results]);
-      fs.writeFile(FILE_NAME, newEmbeddings, (err) => {});
-    }
-
     return results;
   }
 }
@@ -63,15 +53,12 @@ function cosineSimilarity(vecA: any, vecB: any) {
 async function test() {
   const texts = [
     "David Mathy",
-    "Caisson pour bureau David Mathy By Bols",
     // '<div></div><div>* Offre de bienvenue 5% de réduction sur votre 1ère commande avec le code: PROMO5<br></div><div><br></div><div>Un gain de place assuré avec le lit mezzanine.<br></div><div>Résolument contemporain pour une ambiance zen et chaleureuse.<br>Sa finition, couleur cappuccino et les chants en bois huilés, lui confèrent à la fois douceur et robustesse.<br><br>Le panneau fermé d\'un côté permet un gain de place dans la chambre de votre enfant vous pourrez adosser une commode ou un bureau à l\'extérieur et des étagères à l\'intérieur.<br> Sous la mezzanine possibilité de créer un espace de jeu, de travail ou d\'installer un lit supplémentaire.<br>L\'échelle se positionne au choix lors du montage à gauche ou à droite.<br><table border="0" cellpadding="0" cellspacing="0" width="390" style="border-collapse: collapse; width: 390pt;"><colgroup><col width="65" span="6" style="width: 65pt;"></colgroup><tbody><tr height="15" style="height: 15pt;"><td height="15" colspan="4" width="260" style="height: 15pt; width: 260pt;"><b>EXCEPTIONNEL ! Jusqu\'au 31 janvier 2015</b></td><td width="65" style="width: 65pt;"></td><td width="65" style="width: 65pt;"></td></tr><tr height="15" style="height: 15pt;"><td height="15" colspan="6" style="height: 15pt;"><b>A partir de 1000 € D\'ACHAT = 5% DE REMISE-code promo OFFRE5</b></td></tr><tr height="15" style="height: 15pt;"><td height="15" colspan="6" style="height: 15pt;"><b>A partir 1800 € D\'ACHAT = 7% DE REMISE-code promo OFFRE7</b></td></tr></tbody></table></div><div><br></div><div>Hauteur totale 165 cm, hauteur sous sommier 127 cm.<br><br>Afin de respecter la norme de sécurité pour le lit mezzanine, il conviendra de respecter une épaisseur maximale de 17cm (matelas + sommier)<br>Convient à partir de 6 ans<br><br>Étagère vendue séparément<br>Voir notre rubrique Accessoires lit enfant.<br><br>Tous les lits Mathy By Bols peuvent être fabriqués en couchage longueur 200 au même tarif, si vous souhaitez cette dimension validez le modèle 90X190 et précisez : DIMENSION COUCHAGE 90x200 lors du récapitulatif de commande, rubrique 5.MESSAGE.<b style="font-weight: bold;"><b><br><br></b></b><br></div>',
   ];
 
-  const embeddings = await embeddingApi(texts, 100, true);
-  // const file = fs.readFileSync(FILE_NAME, "utf8");
-  // const embeddings = JSON.parse(file) as any[];
+  const embeddings = await embeddingApi(texts, 100);
 
   console.log(cosineSimilarity(embeddings[0], embeddings[1]));
 }
 
-test();
+// test();
