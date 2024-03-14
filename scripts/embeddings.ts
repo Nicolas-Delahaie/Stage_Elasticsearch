d565fbc115f763f6c154ea2c25dc9a127bf75c4d
Nicolas delahaie <n.delahaie@solidpepper.com>
2024-03-14 15:26:17 +0100

    Legers fix : reduction de la taille devenue proportionnelle

--- scripts/embeddings.ts
+++ scripts/embeddings.ts
@@ -1,55 +1,64 @@
 const MODEL_NAME = "text-embedding-3-small";
 
 export async function embeddingApi(texts: string[], dimensions: number) {
-  const CHARS_IN_A_TOKEN = 4; // Moyenne de nombre de caracteres par token
-  const MAX_TOKENS_PER_SECTION = 7000; // Au lieu de 8191 pour etre large
+  const CHARS_IN_A_TOKEN = 10; // Moyenne de nombre de caracteres par token
+  const MAX_TOKENS_PER_SECTION = 8000; // Arrondi de 8191
   const MAX_CHARS_PER_SECTION = MAX_TOKENS_PER_SECTION * CHARS_IN_A_TOKEN;
   const SECTION_RATIO_REDUCTION = 6;
 
   let charCounter = 0;
   let iLastSectionText = 0;
-  let requestFailed = false;
+  let lastRequestFailed = false;
+  let reductionIncrement = undefined;
   let embeddings: number[][] = [];
   while (texts.length !== 0) {
-    if (!requestFailed) {
+    if (!lastRequestFailed) {
       const textLength = texts[iLastSectionText].length;
       charCounter += textLength;
     }
     const isLastElement = iLastSectionText == texts.length - 1;
 
-    if (requestFailed || isLastElement || charCounter > MAX_CHARS_PER_SECTION) {
+    if (lastRequestFailed || isLastElement || charCounter > MAX_CHARS_PER_SECTION) {
       // Tentative de generation d embedding
       const section = texts.slice(0, iLastSectionText + 1);
       const others = texts.slice(iLastSectionText + 1);
-      console.log("DEBUG - Envoi des textes : ", section);
       const res = await requestEmbeddingApi(section, dimensions);
-      /**@todo INTEGRER L AJOUT DANS ELASTIC SEARCH ICI (pour ne pas faire des embeddings pour rien au cas ou une section fasse planter) */
+      // /**@todo INTEGRER L AJOUT DANS ELASTIC SEARCH ICI (pour ne pas faire des embeddings pour rien au cas ou une section fasse planter) */
       if (res.success) {
         // Stockages des embeddings
-        console.log(res.data);
         embeddings.push(...res.data);
 
         // Reinitialisation donnees
         charCounter = 0;
-        requestFailed = false;
+        lastRequestFailed = false;
+        reductionIncrement = undefined;
 
         // Reduction des textes restants a envoyer
         texts = others;
         iLastSectionText = 0;
-        console.log(section.length, " embeddings générés !");
+        console.log("\u2705 ", section.length, " embeddings générés !");
       } else {
         const typeErreur = res.error.type;
+        console.log(res.error);
         if (typeErreur === "invalid_request_error") {
           // Tant que la section ne peut pas etre envoyee, elle est reduite
-          requestFailed = true;
-          iLastSectionText -= Math.ceil(iLastSectionText / SECTION_RATIO_REDUCTION);
-          console.log("Trop grand nombre de tokens pour l'API d'OpenAI : reduction des donnees");
+          lastRequestFailed = true;
+          if (reductionIncrement === undefined) {
+            // Premiere erreur
+            reductionIncrement = Math.round(iLastSectionText / SECTION_RATIO_REDUCTION);
+          }
+          iLastSectionText -= reductionIncrement;
+
+          if (iLastSectionText <= 0) {
+            throw Error('Erreur "' + res.error.type + '" :' + res.error.message);
+          }
+          console.log("<-- Trop grand nombre de tokens pour l'API d'OpenAI : reduction des donnees a envoyer a ", iLastSectionText);
         } else if (typeErreur === "rate_limit_error") {
-          throw Error("Impossible de continuer, nombre de requetes max autorisées par OpenAI atteint");
+          throw Error('Erreur "' + res.error.type + '" :' + "Impossible de continuer, nombre de requetes max autorisées par OpenAI atteint");
         } else if (typeErreur === "internal_server_error " || typeErreur === "service_unavailable") {
-          throw Error("Probleme interne d'OpenAI, impossible de générer les embeddings");
+          throw Error('Erreur "' + res.error.type + '" :' + "Probleme interne d'OpenAI, impossible de générer les embeddings");
         } else {
-          throw Error("Impossible de generer les embeddings OpenAI");
+          throw Error('Erreur "' + res.error.type + '" :' + "Impossible de generer les embeddings OpenAI");
         }
       }
     } else {
@@ -127,22 +136,26 @@ async function testSimiliarite() {
   const texts = [
     "David Mathy",
     // '<div></div><div>* Offre de bienvenue 5% de réduction sur votre 1ère commande avec le code: PROMO5<br></div><div><br></div><div>Un gain de place assuré avec le lit mezzanine.<br></div><div>Résolument contemporain pour une ambiance zen et chaleureuse.<br>Sa finition, couleur cappuccino et les chants en bois huilés, lui confèrent à la fois douceur et robustesse.<br><br>Le panneau fermé d\'un côté permet un gain de place dans la chambre de votre enfant vous pourrez adosser une commode ou un bureau à l\'extérieur et des étagères à l\'intérieur.<br> Sous la mezzanine possibilité de créer un espace de jeu, de travail ou d\'installer un lit supplémentaire.<br>L\'échelle se positionne au choix lors du montage à gauche ou à droite.<br><table border="0" cellpadding="0" cellspacing="0" width="390" style="border-collapse: collapse; width: 390pt;"><colgroup><col width="65" span="6" style="width: 65pt;"></colgroup><tbody><tr height="15" style="height: 15pt;"><td height="15" colspan="4" width="260" style="height: 15pt; width: 260pt;"><b>EXCEPTIONNEL ! Jusqu\'au 31 janvier 2015</b></td><td width="65" style="width: 65pt;"></td><td width="65" style="width: 65pt;"></td></tr><tr height="15" style="height: 15pt;"><td height="15" colspan="6" style="height: 15pt;"><b>A partir de 1000 € D\'ACHAT = 5% DE REMISE-code promo OFFRE5</b></td></tr><tr height="15" style="height: 15pt;"><td height="15" colspan="6" style="height: 15pt;"><b>A partir 1800 € D\'ACHAT = 7% DE REMISE-code promo OFFRE7</b></td></tr></tbody></table></div><div><br></div><div>Hauteur totale 165 cm, hauteur sous sommier 127 cm.<br><br>Afin de respecter la norme de sécurité pour le lit mezzanine, il conviendra de respecter une épaisseur maximale de 17cm (matelas + sommier)<br>Convient à partir de 6 ans<br><br>Étagère vendue séparément<br>Voir notre rubrique Accessoires lit enfant.<br><br>Tous les lits Mathy By Bols peuvent être fabriqués en couchage longueur 200 au même tarif, si vous souhaitez cette dimension validez le modèle 90X190 et précisez : DIMENSION COUCHAGE 90x200 lors du récapitulatif de commande, rubrique 5.MESSAGE.<b style="font-weight: bold;"><b><br><br></b></b><br></div>',
+    "Coucou",
   ];
 
-  const embeddings = await requestEmbeddingApi(texts, 100);
-
-  // console.log(cosineSimilarity(embeddings[0], embeddings[1]));
+  const res = await requestEmbeddingApi(texts, 100);
+  console.log(res);
+  if (res.success) {
+    // console.log(cosineSimilarity(res.data[0], res.data[1]));
+  }
 }
 
 async function test() {
   let texts;
-  texts = [Array(10).fill("cafff").join(" ")];
-  console.log(await embeddingApi(texts, 10));
+  // texts = [...Array(1000).fill("Coucou ça va ou quoiiiii ?"), ...Array(3000).fill("Okkk"), ...Array(1000).fill("Bonjour je m'appelle patate")]; // 1600 x 5 = 8000 tokens
+  // console.log("Resultat : ", (await embeddingApi(texts, 3)).length, " embeddings");
   // if (!embeddings.success) {
   //   console.log(embeddings.error.message.match(/^\D*\d+\D+(\d+)/)?.[1]);
   // }
-
-  texts = ["Coucou moi anticonstitionnellement", "hgbyu", "uhuih", "uhuih"];
+  texts = ["Coucou", "bhiuh", "huighuih", "iljpo", "hju"];
+  texts = texts.slice(5).length;
+  console.log(texts);
 }
 
-test();
+// testSimiliarite();
